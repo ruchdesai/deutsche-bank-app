ed3283d2ccf06de34dde2eb27a69b153a4229862
Ruchit Desai <ruchitdesai84@gmail.com>
2020-05-14 14:47:18 +0100

    moved interface file

---
+++ src/app/services/countries/country.interface.ts
@@ -0,0 +1,42 @@
+export default interface Country {
+    name: string;
+    topLevelDomain: string[];
+    alpha2Code: string;
+    alpha3Code: string;
+    callingCodes: string[];
+    capital: string;
+    altSpellings: string[];
+    region: string;
+    subregion: string;
+    population: number;
+    latlng: number[];
+    demonym: string;
+    area: number;
+    gini: number;
+    timezones: string[];
+    borders: string[];
+    nativeName: string;
+    numericCode: string;
+    currencies: {
+        code: string,
+        name: string,
+        symbol: string
+    }[];
+    languages: {
+        iso639_1: string,
+        iso639_2: string,
+        name: string,
+        nativeName: string
+    }[];
+    translations: any;
+    flag: string;
+    regionalBlocs: {
+        acronym: string,
+        name: string,
+        otherAcronyms: any,
+        otherNames: any
+    }[];
+    cioc: string;
+}
+
+export interface Countries extends Array<Country> {}
